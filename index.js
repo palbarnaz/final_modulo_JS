ce646788e6aa0b25122ea80739dd7b6d719bb0b3
Paola Albarnaz <paola.silva.nh1@gmail.com>
2022-12-29 17:49:43 -0200

    Iniciando projeto final do modulo js

---
+++ index.js
@@ -0,0 +1,4 @@
+// document.getElementById("link-conta").addEventListener("click", function(){
+//   alert('O usuário clicou no link criar conta!')
+//   document.querySelector('tbody').prepend(`<tr><td>${id}</td><td></td></tr>`)
+// })
