02f28abc7edc1b63f03d952dc886531144618b03
Paola Albarnaz <paola.silva.nh1@gmail.com>
2023-01-12 13:07:56 -0200

    removendo arquivos antigos

--- index.js
+++
@@ -1,4 +0,0 @@
-// document.getElementById("link-conta").addEventListener("click", function(){
-//   alert('O usuário clicou no link criar conta!')
-//   document.querySelector('tbody').prepend(`<tr><td>${id}</td><td></td></tr>`)
-// })
