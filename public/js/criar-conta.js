e16e7a2253d398608ea8b1dfd35bc744cd09931d
Paola Albarnaz <paola.silva.nh1@gmail.com>
2023-01-15 14:37:27 -0200

    finalizando projeto final

---
+++ public/js/criar-conta.js
@@ -0,0 +1,71 @@
+// CRIAR CONTA
+const create = document.getElementById('create-form');
+
+const usuario = localStorage.getItem('usuarioLogado');
+document.addEventListener('DOMContentLoaded', ()=> {
+  
+  if (usuario) {
+    window.location.href = 'home.html'
+  }
+})
+
+  create.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const email = document.getElementById('email-input').value;
+    const password = document.getElementById('senha-input').value;
+    const password2 = document.getElementById('senha-input2').value;
+
+    const verificado = validarLogin(email, password, password2);
+
+    if (verificado) {
+      let usuario = {
+        login: email,
+        password: password,
+        listaRecados: [],
+      };
+      salvarUsuario(usuario);
+      alert('Conta Criada com Sucesso!');
+    }
+
+    create.reset();
+  });
+
+
+
+function validarLogin(email, senha1, senha2) {
+  if (email.length < 5) {
+    alert('Preencha o campo com um e-mail válido!');
+    return false;
+  }
+  if (senha1 !== senha2) {
+    alert('Senhas não batem. Verifique o valor digitado.');
+    return false;
+  }
+  if (senha1.length < 4) {
+    alert('Preencha a senha com no mínimo 4 digitos!');
+    return false;
+  }
+  const listaUsuarios = JSON.parse(localStorage.getItem('usuarios'));
+
+  if (listaUsuarios !== null) {
+    const existe = listaUsuarios.some((valor) => valor.login == email);
+
+    if (existe) {
+      alert('Usuário já cadastrado!');
+      return;
+    }
+  }
+  return true;
+}
+
+
+function salvarUsuario(objeto) {
+  let listaUsuarios = localStorage.getItem('usuarios');
+  if (!listaUsuarios) {
+    localStorage.setItem('usuarios', JSON.stringify([objeto]));
+  } else {
+    listaUsuarios = JSON.parse(listaUsuarios);
+    listaUsuarios.push(objeto);
+    localStorage.setItem('usuarios', JSON.stringify(listaUsuarios));
+  }
+}
--- public/js/home.js
+++ public/js/home.js
@@ -1,14 +1,45 @@
-const listaRecados = []
 const formularioRecados = document.getElementById('formulario-recados')
 const tbody = document.getElementById('recados')
 
 
-// buscar usuario no sistema 
+document.addEventListener('DOMContentLoaded', ()=> {
+  
+  if (!usuario) {
+    window.location.href = 'index.html'
+  } else {
+    atualizarTabela()
+  }
+  
+})
 
+// buscar lista no sistema 
 
+let listaUsuarios = JSON.parse(localStorage.getItem('usuarios'));
 
+// buscar usuario logado no sistema
+const usuario = localStorage.getItem('usuarioLogado');
 
+// Informações do usuário logado
+let infoUsuario = listaUsuarios.find((valor) => valor.login === usuario);
 
+function salvarRecado(objeto) {
+   
+infoUsuario.listaRecados.push(objeto)
+
+  const usuarios = listaUsuarios.map((usuario) => {
+    if (usuario.login === infoUsuario.login) {
+     
+     return infoUsuario;  
+  
+    }
+    return usuario
+  })
+  
+  // pegando o retorno do map, mando o objeto da pessoa logada, já atualizado, para o LocalStorage.
+  let mandarObjeto = JSON.stringify(usuarios) 
+ 
+  localStorage.setItem('usuarios', mandarObjeto)
+}
 formularioRecados.addEventListener('submit', (e)=> {
   e.preventDefault()
   
@@ -20,8 +51,8 @@ formularioRecados.addEventListener('submit', (e)=> {
      descricao: descricao,
      detalhe: detalhe,
   }
-  
-  listaRecados.push(novoRecado)
+  salvarRecado(novoRecado)
+
   formularioRecados.reset()
 
   atualizarTabela()
@@ -31,14 +62,89 @@ formularioRecados.addEventListener('submit', (e)=> {
 function atualizarTabela(){
   tbody.innerHTML = ''
 
-  listaRecados.forEach((recado,index) => {
+  infoUsuario.listaRecados.forEach((recado,index) => {
     tbody.innerHTML += `
-    <tr>
+    <tr id="${index}" >
           <th>${index+1}</th>
           <td>${recado.descricao}</td>
           <td>${recado.detalhe}</td>
-          <td><button class="button-apagar">Apagar</button><button class="button-editar">Editar</button></td>
+          <td><button class="button-apagar" onclick="apagarRegistro(${index})" >Apagar</button><button class="button-editar"  onclick="editarRegistro(${index})" >Editar</button></td>
     </tr>
   `
   })
+}
+
+function apagarRegistro(indice) {
+  // remover da lista de registros
+  let listaAtualizada = infoUsuario.listaRecados.filter((valor, index)=> index != indice)
+
+
+  let usuarios = listaUsuarios.map((usuario) => {
+    if (usuario.login === infoUsuario.login) {
+      infoUsuario.listaRecados = listaAtualizada 
+     return infoUsuario;  
+   
+    }
+    return usuario
+  })
+  
+ localStorage.setItem('usuarios', JSON.stringify(usuarios) ) 
+  
+  // atualizar o localstorage
+
+  // remover do html
+  let trRemover = document.getElementById(indice)
+  trRemover.remove()
+  atualizarTabela()
+  
+}
+function sairDoSistema(){
+  localStorage.removeItem('usuarioLogado')
+  window.location.href = "index.html";
+}
+
+function buscarRecado(indice){
+  let recado = infoUsuario.listaRecados.find((valor, index)=> index === indice)
+  return recado
+}
+
+function editarRegistro(indice){
+  const linhaEditar = document.getElementById(indice)
+  const recado = buscarRecado(indice)
+
+   linhaEditar.innerHTML = `
+   
+      <tr>
+          <th>${indice+1}</th>
+          <td><input class="editar-descricao" type="text" name="descricao" id="descricao-${indice}" value="${recado.descricao}"/></td>
+          <td> <input class="editar-detalhe" type="text" name="detalhe" id="detalhe-${indice}" value="${recado.detalhe}" /></td>
+          <td><button class="button-editar"  onclick="editarRecado(${indice})">Editar</button></td>
+      </tr>
+   `
+}
+function editarRecado(indice){
+  
+  const novaDescricao = document.getElementById(`descricao-${indice}`).value
+  const novoDetalhe = document.getElementById(`detalhe-${indice}`).value
+  
+  const recadoEditar = buscarRecado(indice)
+  recadoEditar.descricao =  novaDescricao
+  recadoEditar.detalhe = novoDetalhe
+
+  atualizarTabela()
+
+  infoUsuario.listaRecados[indice] = recadoEditar
+
+  let usuarios = listaUsuarios.map((usuario) => {
+    if (usuario.login === infoUsuario.login) {
+       infoUsuario.listaRecados
+     return infoUsuario;  
+   
+    }
+    return usuario
+  })
+  
+
+ localStorage.setItem('usuarios', JSON.stringify(usuarios) ) 
+ 
 }
